c0759a2870650f5ebe8cc8d478ce20c8b87e4f64
Ryan Burke <ryanb-93@hotmail.co.uk>
2014-11-13 12:11:20 +0000

    Uninstall now deletes config files and removes dependencies.

--- layout/usr/libexec/cydelete/uninstall_dpkg.sh
+++ layout/usr/libexec/cydelete/uninstall_dpkg.sh
@@ -2,7 +2,7 @@
 export PATH=/usr/bin:/bin:/sbin:/usr/sbin
 killall -9 MobileCydia
 sleep 3
-apt-get -y remove $1 2>/tmp/aptoutput 1>/dev/null
+apt-get -y --purge autoremove $1 2>/tmp/aptoutput 1>/dev/null
 exitcode=$?
 rm -rf /tmp/aptoutput &> /dev/null
 exit $exitcode
